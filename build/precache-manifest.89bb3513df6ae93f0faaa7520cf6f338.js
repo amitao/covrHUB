04305d88c251947d63c13c1eacb61ee2e8dd43a0
Amie Thao <amie.c.thao@gmail.com>
2019-01-25 10:15:22 -0600

    add build

---
+++ build/precache-manifest.89bb3513df6ae93f0faaa7520cf6f338.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f922d1821428a96279b8",
+    "url": "/static/css/main.1fa94aad.chunk.css"
+  },
+  {
+    "revision": "f922d1821428a96279b8",
+    "url": "/static/js/main.f922d182.chunk.js"
+  },
+  {
+    "revision": "857401583057d72720e9",
+    "url": "/static/js/1.85740158.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c505bed2d7b043176bc3716e7941cdb0",
+    "url": "/index.html"
+  }
+];
